e806190640b812ee3d7e048aaf39054e715a6d40
Nathan Lane <nathan.lane@overstock.com>
2019-04-12 16:56:10 -0600

    Add more functions

--- js/functional.js
+++ js/functional.js
@@ -10,3 +10,14 @@ isPalindrome = (str, c) => { return (present(str) && (typeof str === 'string' ||
 trim1 = (str) => { return not(str) || str == "" || str.slice(1, length(str) - 1); }
 
 isPalindrome = (str) => { return present(str) && (typeof str === 'string' || str instanceof String) && (length(str) < 2 || compare(str[0], str[(length(str) - 1)]) && (trim1(str) == "" || isPalindrome(trim1(str)))); }
+
+
+isArray = (arr) => { return Array.isArray(arr); }
+arrayLength = (arr) => { return (present(arr)  && isArray(arr) && arr.length) || 0; }
+isGreater = (x, y) => { return x > y; }
+swap = ([x, y]) => { return [y, x]; }
+sliceArray = (arrX, firstIndex, count) => { return arrX.slice(firstIndex, count); }
+concatArray = (arrX, arrY) => { return arrX.concat(arrY); }
+
+// bubbleSort = (arrX, firstIndex) => { return (arrayLength(arrX) > 1) || arrX; }
+// sort = (arrX, firstIndex) => { return isGreater(arrX[firstIndex], arrX[(firstIndex + 1)]) ? sort(concatArray(swap(sliceArray(arrX, 0, 2)), sliceArray(arrX, 2, undefined)), firsstIndex + 1) : arrX; }
