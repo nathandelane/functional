ee443c75b1677ffb842e90949d5ae55916e481aa
Nathan Lane <nathan.lane@overstock.com>
2019-09-12 17:11:02 -0600

    Add gameObjects collection, add more screen wrapping logic.

--- js/games/asteroids/asteroids.js
+++ js/games/asteroids/asteroids.js
@@ -1,19 +1,17 @@
 
-const start = (width, height) => {
-    var canvas = document.getElementById("canvas0");
-
-    canvas.width = width;
-    canvas.height = height;
-
-    var c = canvas.getContext("2d");
-
-    return c;
-}
-
 const randomInt = (max) => {
     return Math.random() * max;
 }
 
+/**
+ * Ellipse object, which is the base for an Asteroid.
+ * @param {CanvasRenderingContext2D} g2d 
+ * @param {number} centerX 
+ * @param {number} centerY 
+ * @param {number} radiusX 
+ * @param {number} radiusY 
+ * @param {string} color 
+ */
 function Ellipse(g2d, centerX, centerY, radiusX, radiusY, color) {
 
     let ellipse = Object.create(Ellipse.prototype);
@@ -55,6 +53,14 @@ Ellipse.prototype.render = function () {
     this.g2d.stroke();
 }
 
+/**
+ * 
+ * @param {CanvasRenderingContext2D} g2d 
+ * @param {object} centerPoint 
+ * @param {object} radiusDims 
+ * @param {number} numberOfSides 
+ * @param {string} color 
+ */
 function Asteroid(g2d, centerPoint, radiusDims, numberOfSides, color) {
 
     let asteroid = Object.create(Asteroid.prototype);
@@ -84,8 +90,6 @@ function Asteroid(g2d, centerPoint, radiusDims, numberOfSides, color) {
 }
 
 Asteroid.prototype.render = function () {
-    console.log("steps: " + this.steps);
-
     this.g2d.moveTo(this.centerPoint.x, this.centerPoint.y);
     this.g2d.beginPath();
 
@@ -112,30 +116,36 @@ Asteroid.prototype.translate = function (diffX, diffY) {
     this.centerPoint.x = this.centerPoint.x + diffX;
     this.centerPoint.y = this.centerPoint.y + diffY;
 
-    if ((this.centerPoint.x - this.radiusDims.x) > 640) {
-        this.centerPoint.x = 0;
+    if ((this.centerPoint.x - this.radiusDims.width) > 640) {
+        this.centerPoint.x = 0 - this.radiusDims.width;
     }
-    if ((this.centerPoint.y - this.radiusDims.y) > 480) {
-        this.centerPoint.y = 0;
+    else if ((this.centerPoint.x + this.radiusDims.width) < 0) {
+        this.centerPoint.x = 640 + this.radiusDims.width
+    }
+
+    if ((this.centerPoint.y - this.radiusDims.height) > 480) {
+        this.centerPoint.y = 0 - this.radiusDims.height;
+    }
+    else if ((this.centerPoint.y + this.radiusDims.height) < 0) {
+        this.centerPoint.y = 480 + this.radiusDims.height;
     }
 }
 
 Asteroid.prototype.rotate = function (degrees) {
-    console.log("degrees: " + degrees);
-
     var radiansToTranslate = degrees * Math.PI / 180;
     var translatedPoints = [];
-
-    console.log("radians: " + radiansToTranslate);
 
     for (var i = 0; i < this.steps.length; i++) {
         var nextPoint = this.steps[i];
         var calculation = (nextPoint + radiansToTranslate);
 
-        console.log("nextPoint: " + nextPoint + ", calculation: " + calculation);
-
         if (calculation > (2 * Math.PI)) {
             var difference = calculation - (2 * Math.PI);
+
+            calculation = difference;
+        }
+        else if (calculation < 0) {
+            var difference = (2 * Math.PI) + calculation;
 
             calculation = difference;
         }
@@ -148,6 +158,17 @@ Asteroid.prototype.rotate = function (degrees) {
 
 /////////////////////////////////////////////////////////////////////////
 // Start of application
+const start = (width, height) => {
+    var canvas = document.getElementById("canvas0");
+
+    canvas.width = width;
+    canvas.height = height;
+
+    var c = canvas.getContext("2d");
+
+    return c;
+}
+
 var g2d = start(640, 480);
 
 // y = new Ellipse(g2d, 100, 100, 50, 50, "red");
@@ -160,27 +181,53 @@ var lastTime = (new Date()).getTime();
 var currentTime = 0;
 var delta = 0;
 
-var numberOfFrames = 30;
+var gameObjects = [];
+gameObjects.push(
+    { 
+        "renderable": new Asteroid(g2d, { "x": 100, "y": 100 }, { "width": 21, "height": 31}, 11, "gray"), 
+        "translation": { "x": 0.7, "y": 0.3 },
+        "rotation": 0.5
+    },
+    { 
+        "renderable": new Asteroid(g2d, { "x": 300, "y": 320 }, { "width": 19, "height": 18}, 13, "red"), 
+        "translation": { "x": -0.9, "y": 0.2 },
+        "rotation": -0.5
+    }
+);
 
-var x = new Asteroid(g2d, { "x": 100, "y": 100 }, { "width": 21, "height": 31}, 11, "gray");
+// var x = new Asteroid(g2d, { "x": 100, "y": 100 }, { "width": 21, "height": 31}, 11, "gray");
 
-function update() {
-    x.translate(0.7, 0.3);
-    x.rotate(0.5);
+const update = () => {
+    for (var i = 0; i < gameObjects.length; i++) {
+        var nextGameObject = gameObjects[i];
+        var nextRenderable = nextGameObject.renderable;
+        var nextTranslation = nextGameObject.translation;
+        var nextRotation = nextGameObject.rotation;
+
+        nextRenderable.translate(nextTranslation.x, nextTranslation.y);
+        nextRenderable.rotate(nextRotation);
+    }
+    // x.translate(0.7, 0.3);
+    // x.rotate(0.5);
 }
 
-function clearCanvas() {
+const clearCanvas = () => {
     g2d.clearRect(0, 0, 640, 480);
-    console.log("Cleared");
 }
 
-function draw() {
+const draw = () => {
     clearCanvas();
 
-    x.render();
+    for (var i = 0; i < gameObjects.length; i++) {
+        var nextGameObject = gameObjects[i];
+        var nextRenderable = nextGameObject.renderable;
+
+        nextRenderable.render();
+    }
+    // x.render();
 }
 
-function loop() {
+const loop = () => {
     window.requestAnimationFrame(loop);
 
     currentTime = (new Date()).getTime();
@@ -191,8 +238,6 @@ function loop() {
         draw();
 
         lastTime = currentTime - (delta % interval);
-
-        numberOfFrames++;
     }
 }
 
